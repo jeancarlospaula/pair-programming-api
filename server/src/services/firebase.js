ad066098a0cb2224c89c9775bd28e33c97c1e994
jean.depaula <jean.depaula@obraprimaweb.com.br>
2022-03-25 21:47:35 -0300

    Updated firebase config key

--- server/src/services/firebase.js
+++ server/src/services/firebase.js
@@ -8,7 +8,7 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replaceAll(/\\n/gm, '\n'),
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\n/g, '\n'),
   }),
   storageBucket: bucketURL
 });
